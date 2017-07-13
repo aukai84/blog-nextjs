8fa4d1abb380d948bcf8e1fd3620dcc644798b04
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-07-13 07:58:19 -1000

    Added an api route to retrieve individual posts

--- server/server.js
+++ server/server.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 const Model = require('./models/Post');
 
-
-
 app.get('/api/post', (req, res) => {
   new Model.Post()
 		.fetchAll()
+		.then(post => {
+      res.json(post)
+    })
+		.catch(function (error) {
+			 console.log(error);
+			 res.send('An error occured');
+		});
+});
+
+app.get('/api/post/:id', (req, res) => {
+  new Model.Post({
+    'id': req.params.id
+  })
+		.fetch()
 		.then(post => {
       res.json(post)
     })
